d6976a9a23b70e88ae198d543095334439d9f1fd
nguyenit67 <dehungmanh@gmail.com>
2021-01-14 11:33:13 +0700

    first try at Book Copy Update

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -134,11 +134,77 @@ module.exports = new (class {
 
   // Display bookinstance update form on GET.
   bookinstance_update_get(req, res, next) {
-    res.send("NOT IMPLEMENTED: bookinstance update GET");
+    BookInstance.findById(req.params.id)
+      .populate("book")
+      .exec((err, bookinstance) => {
+        if (err) {
+          return next(err);
+        }
+
+        res.render("bookinstance_form", {
+          title: "Edit Book Copy",
+          bookinstance,
+        });
+      });
   }
 
   // Handle bookinstance update on POST.
-  bookinstance_update_post(req, res, next) {
-    res.send("NOT IMPLEMENTED: bookinstance update POST");
+  get bookinstance_update_post() {
+    return [
+      // Validate and sanitise fields.
+      body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+      body("book").custom((value) => {
+        return Book.findById(value).then((book) => {
+          if (!book) {
+            return Promise.reject(new Error("Book not found! :<"));
+          }
+        }).catch((err) => {
+          return Promise.reject(new Error("Book not found! :<"));
+        });
+      }),
+      body("imprint", "Imprint must be specified").trim().isLength({ min: 1 }).escape(),
+      body("status").escape(),
+      body("due_back", "Invalid date").optional({ checkFalsy: true }).isISO8601().toDate(),
+      (req, res, next) => {
+        const errors = validationResult(req);
+        const bookinstance = new BookInstance(
+          { _id: req.params.id,
+            book: req.body.book,
+            imprint: req.body.imprint,
+            status: req.body.status,
+            due_back: req.body.due_back,
+          });
+
+        if (errors.isEmpty()) {
+          BookInstance.findByIdAndUpdate(req.params.id, bookinstance, {}, (findNUpdErr, thebookinstance) => {
+            if (findNUpdErr) {
+              return next(findNUpdErr);
+            }
+            res.redirect(thebookinstance.url);
+          });
+        }
+        else {
+          Book.find({}, "title")
+            .exec((err, book_list) => {
+              if (err) { return next(err); }
+
+              let selected_book = bookinstance.book?.toString();
+              if ("book" in errors.mapped()) {
+                selected_book = null;
+              }
+              // Successful, so render.
+              res.render("bookinstance_form",
+                { title: "Create BookInstance",
+                  book_list,
+                  bookinstance,
+                  selected_book,
+                  errors: errors.array(),
+                  status_enum: BookInstance.schema.path("status").enumValues,
+                });
+            });
+        }
+      },
+
+    ];
   }
 })();
