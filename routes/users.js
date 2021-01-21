c6a8243784d740910f9fa35dd00638dd2247781d
nguyenit67 <dehungmanh@gmail.com>
2021-01-21 15:28:27 +0700

    re-format code

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
 
 router.get("/meow", function (req, res, next) {
-  res.send("You're so meow")
+  res.send("You're so meow");
 });
