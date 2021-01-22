18ac515877dc36033d529e3a206f9516dc095bbf
nguyenit67 <dehungmanh@gmail.com>
2021-01-22 11:16:21 +0700

    add usersRouter.js

---
+++ routes/usersRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+const user_controller = require("../controllers/userController");
+
+router.get("/register", user_controller.register_get);
+
+router.post("/register", user_controller.register_post);
+
+router.get("/login", user_controller.login_get);
+
+router.post("/login", user_controller.login_post);
+
+module.exports = router;
