fe9ca35ad2440ee827e155d02e9233edde38df29
nguyenit67 <dehungmanh@gmail.com>
2021-01-23 10:54:22 +0700

    change model properties

--- models/user.js
+++ models/user.js
@@ -2,19 +2,12 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  firstName: { type: String, required: true, maxlength: 100 },
-  lastName:  { type: String, required: true, maxlength: 100 },
-  email:     { type: String, required: true, unique: true },
-  password:  { type: String, required: true },
-});
-
-// Virtual for User's full name
-UserSchema.virtual("name").get(function () {
-  return `${this.lastName}, ${this.firstName}`;
+  name:     { type: String, required: true, maxlength: 100 },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 UserSchema.virtual("url").get(function () {
-  if (this) { console.log({ UserSchema }); }
   return `/catalog/User/${this._id}`;
 });
 
