31f0c2efb320703ee741e3d3c02df8e441a5059c
nguyenit67 <dehungmanh@gmail.com>
2021-01-23 10:56:13 +0700

    change (useless) users.js -> usersRouter.js

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const user_controller = require("../controllers/userController");
 
+router.get("/list/all", user_controller.user_list);
+
 router.get("/register", user_controller.register_get);
 
 router.post("/register", user_controller.register_post);
